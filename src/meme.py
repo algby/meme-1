70d10fe2fde99eb1aa9772138431c941c68f3b15
Jamie Webb <code@jwebb.cc>
2010-11-14 11:55:45 +0000

    Accept a filename on the command line

--- src/meme.py
+++ src/meme.py
@@ -3,10 +3,19 @@
 import sys, pygtk, gtk
 pygtk.require('2.0')
 
-from meme.ui import *
+import sys
+from meme.ui import MemeGui
+from meme.model import Model
+from meme.io import read_native
 
 if __name__ == "__main__":
-	gui = MemeGui()
+	if len(sys.argv) == 1:
+		gui = MemeGui()
+	elif len(sys.argv) == 2:
+		model = Model(read_native(sys.argv[1]))
+		gui = MemeGui(model)
+	else:
+		print "Usage: %s [file]" % sys.argv[0]
 	gtk.main()
 
 # vim:sw=4 ts=4
